45f4eca5dcce780b39743e59cd9ba43bbfa5e573
Sharshit24 <vermasumitharshit@gmail.com>
2024-12-17 22:34:02 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [light, setLight] = useState('red');
+
+  const lights = ['red', 'green', 'yellow'];
+  const timings = {
+    red: 6000,
+    green: 3000,
+    yellow: 6000,
+  };
+
+  const lightColors = {
+    red: 'bg-red-500',
+    green: 'bg-green-500',
+    yellow: 'bg-yellow-500',
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLight((prev) => lights[(lights.indexOf(prev) + 1) % lights.length]);
+    }, timings[light]);
+
+    return () => clearTimeout(timer);
+  }, [light]);
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-between items-center w-24 h-72 rounded-lg p-2 bg-black">
+          {lights.map((col) => (
+            <div
+              key={col}
+              className={`w-full h-20 rounded-full ${light === col ? lightColors[col] : 'bg-gray-500'} shadow-lg`}
+              aria-label={col}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
